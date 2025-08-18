425af18ccca23d9ccd6c0931c1038c2511abb376
sazzadshakhbd <sazzadshakhbd@gmail.com>
2025-08-18 23:37:21 +0600

    setup project environment

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// Import Bootstrap CSS (compiled version)
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Import Bootstrap JS (if needed)
+import "bootstrap";
+
+// Import your custom SCSS
+import "../scss/style.scss";
